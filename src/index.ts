54ccf178cdc43edf8acc1771257c3f1343216615
uk-taniyama <uk.taniyama@gmail.com>
2022-08-29 01:08:04 +0900

    Generate d.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { ArcElement, Chart } from 'chart.js';
 
 import GaugeController from './controllers/controller.gauge';
 
+export * from './controllers/controller.gauge';
+
 Chart.register(GaugeController);
 Chart.register(ArcElement);
